660c21023931fea61318d806e4ec4b20c90e4bd6
Kris <kris.dziedzic@neos.co.uk>
2020-09-26 16:24:47 +0200

    Setup dashbaord screen

--- lib/screens/dashbaord_screen.dart
+++ lib/screens/dashbaord_screen.dart
@@ -3,12 +3,36 @@ import 'package:flutter/material.dart';
 class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dashbaord"),
-      ),
       body: Center(
-        child: Text("Dashboard Screen"),
+        child: Container(
+          alignment: Alignment.bottomCenter,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/landscape_vector.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Container(
+            height: screenHeight * (6 / 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(24),
+                topLeft: Radius.circular(24),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(25, 0, 0, 0),
+                  offset: Offset(0.0, -2.0),
+                  blurRadius: 4.0,
+                ),
+              ],
+              color: Color.fromARGB(240, 255, 255, 255),
+            ),
+          ),
+        ),
       ),
     );
   }

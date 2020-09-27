c035d627be1dfbbf3048b813863f7bdd3db4b859
Kris <kris.dziedzic@neos.co.uk>
2020-09-27 19:09:12 +0200

    Add weather details section

--- lib/components/temperature_info/temperaute_info.dart
+++ lib/components/temperature_info/temperaute_info.dart
@@ -1,21 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/enums/temperature_info_type.dart';
 
 class TemperatureInfo extends StatelessWidget {
+  TemperatureInfoType viewType;
+  String data;
+
+  TemperatureInfo({this.viewType, this.data});
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(16),
-      child: Row(
+      child: Column(
         children: [
-          Image.asset(""),
-          Column(
-            children: [
-              Text("Preasure"),
-              Text("1000 hPa"),
-            ],
+          Image.asset(getAssetPath()),
+          Text(
+            getLabel(),
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          Text(
+            data,
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ],
       ),
     );
+  }
+
+  String getAssetPath() {
+    String assetUrl;
+    switch (viewType) {
+      case TemperatureInfoType.PREASURE:
+        assetUrl = "assets/images/wi-barometer.png";
+        break;
+      case TemperatureInfoType.HUMIDITY:
+        assetUrl = "assets/images/wi-humidity.png";
+        break;
+      case TemperatureInfoType.WIND:
+        assetUrl = "assets/images/wi-strong-wind.png";
+        break;
+      case TemperatureInfoType.SUNRISE:
+        assetUrl = "assets/images/time_low.png";
+        break;
+      case TemperatureInfoType.SUNSET:
+        assetUrl = "assets/images/time_high.png";
+        break;
+      case TemperatureInfoType.DAY_TIME:
+        assetUrl = "assets/images/total_time.png";
+        break;
+    }
+    return assetUrl;
+  }
+
+  String getLabel() {
+    String label;
+    switch (viewType) {
+      case TemperatureInfoType.PREASURE:
+        label = "Preasure";
+        break;
+      case TemperatureInfoType.HUMIDITY:
+        label = "Humidity";
+        break;
+      case TemperatureInfoType.WIND:
+        label = "Wind";
+        break;
+      case TemperatureInfoType.SUNRISE:
+        label = "Sunrise";
+        break;
+      case TemperatureInfoType.SUNSET:
+        label = "Sunset";
+        break;
+      case TemperatureInfoType.DAY_TIME:
+        label = "Day time";
+        break;
+    }
+    return label;
   }
 }
--- lib/screens/dashbaord_screen.dart
+++ lib/screens/dashbaord_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/repositories/weather/weather_repository.dart';
+import '../components/temperature_info/temperaute_info.dart';
+import '../enums/temperature_info_type.dart';
+import '../repositories/weather/weather_repository.dart';
 
 import '../components/min_max_temperature/min_max_temperature_component.dart';
 
@@ -38,36 +40,86 @@ class DashboardScreen extends StatelessWidget {
               ],
               color: Color.fromARGB(240, 255, 255, 255),
             ),
-            child: Padding(
-              padding: EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  Text(
-                    "55",
-                    style: TextStyle(
-                      fontSize: 64,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      MinMaxTemperatureComponent(
-                        temperature: "25",
-                        isMin: true,
-                      ),
-                      MinMaxTemperatureComponent(
-                        temperature: "25",
-                        isMin: false,
-                      ),
-                    ],
-                  )
-                ],
+            child: WeatherInfoSection(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WeatherInfoSection extends StatelessWidget {
+  const WeatherInfoSection({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Text(
+              "55",
+              style: TextStyle(
+                fontSize: 64,
               ),
             ),
-          ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                MinMaxTemperatureComponent(
+                  temperature: "25",
+                  isMin: true,
+                ),
+                MinMaxTemperatureComponent(
+                  temperature: "25",
+                  isMin: false,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TemperatureInfo(
+                  viewType: TemperatureInfoType.PREASURE,
+                  data: "1000 hPa",
+                ),
+                TemperatureInfo(
+                  viewType: TemperatureInfoType.HUMIDITY,
+                  data: "39%",
+                ),
+                TemperatureInfo(
+                  viewType: TemperatureInfoType.WIND,
+                  data: "23 km/h",
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TemperatureInfo(
+                  viewType: TemperatureInfoType.SUNRISE,
+                  data: "06:00",
+                ),
+                TemperatureInfo(
+                  viewType: TemperatureInfoType.SUNSET,
+                  data: "21:00",
+                ),
+                TemperatureInfo(
+                  viewType: TemperatureInfoType.DAY_TIME,
+                  data: "15h",
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
